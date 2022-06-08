5812c1c22317c83f85862abab852acd71208f1a7
MaxiHBTN <4387@holbertonschool.com>
2022-06-08 17:49:48 +0000

    Task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ * @array: array of integers
+ * @size: len of the array.
+ **/
+
+void bubble_sort(int *array, size_t size)
+{
+	int swap = 1; /* It becomes 0 when there is not swap */
+	int tmp = 0;
+	size_t i = 0;
+
+	while (swap == 1)
+	{
+		swap = 0;
+		for (i = 0; i < (size - 1); i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+	}
+}
