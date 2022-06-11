e1228dc6ec37ad4e7253773963bf7aa334f21ad4
agusfl <agusflom@hotmail.com>
2022-06-11 13:36:25 +0000

    task 5 structure

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - The algorithm extends bubble sort by operating in two
+ * directions
+ * @list: doubly linked list to be ordered
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+}
