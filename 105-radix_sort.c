92d7f1f3ac4e008ccc3e6c387dd01d82ec8b6168
agusfl <agusflom@hotmail.com>
2022-06-11 13:57:01 +0000

    empty structure - task 9

---
+++ 105-radix_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * radix_sort - is a non-comparative sorting algorithm. It avoids comparison by
+ * creating and distributing elements into buckets according to their radix.
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void radix_sort(int *array, size_t size)
+{
+	print_array(array, size);
+}
