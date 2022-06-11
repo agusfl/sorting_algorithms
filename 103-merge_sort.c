4ac06c8b328f951d7c24d4abd58c4367a4a7b107
agusfl <agusflom@hotmail.com>
2022-06-11 13:51:22 +0000

    task 7 structure with an empty print for the checker

---
+++ 103-merge_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * merge_sort - is a divide-and-conquer, general-purpose, and comparison-based
+ * sorting algorithm.
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void merge_sort(int *array, size_t size)
+{
+	print_array(array, size);
+}
