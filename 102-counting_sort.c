319de84a10b5652e0d87378aebc46ca2944a285d
agusfl <agusflom@hotmail.com>
2022-06-11 13:40:46 +0000

    6 task structure

---
+++ 102-counting_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * counting_sort - is an algorithm for sorting a collection of objects
+ * according to keys that are small positive integers; that is, it is an
+ * integer sorting algorithm.
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void counting_sort(int *array, size_t size)
+{
+}
