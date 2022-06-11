40ba81f7e4c10e64217f91a61f2125245f0f0120
agusfl <agusflom@hotmail.com>
2022-06-11 14:00:09 +0000

    empty structure for task 10

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * bitonic_sort - is a parallel algorithm for sorting. It is also used as a
+ * construction method for building a sorting network.
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void bitonic_sort(int *array, size_t size)
+{
+	print_array(array, size);
+}
