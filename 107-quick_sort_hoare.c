78f919c940f522127191d1cb1c1e155848ee6a78
agusfl <agusflom@hotmail.com>
2022-06-11 14:06:49 +0000

    empty structure for 11 task

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * quick_sort_hoare - implementation of quick sort algorithm with the Hoare
+ * partition scheme.
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void quick_sort_hoare(int *array, size_t size)
+{
+	print_array(array, size);
+}
