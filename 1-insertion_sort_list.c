924a65c9c0c33e43c69ea1dd48436861d43ad914
MaxiHBTN <4387@holbertonschool.com>
2022-06-12 13:38:45 +0000

    Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *swap = NULL;
 
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL)
 		return;
 
 	while (current != NULL)
